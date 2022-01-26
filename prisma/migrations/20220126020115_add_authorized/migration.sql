9cbe5de7ca0b124e923591b9b222b0d590352865
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-26 00:16:13 -0300

    Auth routes done

---
+++ prisma/migrations/20220126020115_add_authorized/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `authorized` BOOLEAN NOT NULL DEFAULT false;
--- src/controllers/Auth/LoginController.ts
+++ src/controllers/Auth/LoginController.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from 'express';
-
+import { prismaClient } from '../../database/prismaClient';
+import bcrypt from 'bcrypt';
+import createToken from '../../middlewares/createToken';
 export class LoginController {
   static async login (req: Request, res: Response) {
-    return res.json({message: "Rota de login!"});
-  }
+    const { email, password } = req.body;
 
+    if(!email) {
+      return res.status(401).json({message: "Digite o email!"});
+    }
+    if(!password) {
+      return res.status(401).json({message: "Digite a senha!"});
+    }
+
+    const user = await prismaClient.user.findUnique({where: {email: email}});
+
+    if(!user) {
+      return res.status(401).json({message: "Não existe usuário com este email!"});
+    }
+
+    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+    if(!passwordsMatch) {
+      return res.status(401).json({message: "Senha inválida!"});
+    }
+
+    if(!user.authorized) {
+      return res.status(401).json({
+        message: "Você não está autorizado! Aguarde autorização do admin",
+        user_autorization: user.authorized
+      });
+    }
+
+    return await createToken(user, req, res);
+
+  }
 }
