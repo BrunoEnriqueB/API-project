f9926467c689b8c94d5246b4a777e4aafe5acb8e
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-02-18 13:19:28 -0300

    Image validation

--- src/controllers/Admin/ProductsController.ts
+++ src/controllers/Admin/ProductsController.ts
@@ -7,16 +7,16 @@ type reqBody = {
   description: string, 
   price: string, 
   quantity: string, 
+  category: string,
   bar_code: string
 }
 
 export class ProductsController {
   static async addProduct(req: Request, res: Response) {
-    if(!req.file) {
-      return res.status(401).json({message: 'Adicione uma imagem!'});
-    }
-    const image = req.file;
-    const { name, description, price, quantity, bar_code }: reqBody = req.body;
+    const { name, description, price, quantity, category, bar_code }: reqBody = req.body;
+    if(!name || !description || !price || !quantity || !category || !bar_code && req.file ) {
+      fs.unlinkSync(`src/public/images/products/${req.file?.filename}`);
+    } 
     if(!name) {
       return res.status(401).json({message: 'Digite um nome para o produto!'});
     }
@@ -29,13 +29,22 @@ export class ProductsController {
     if(!quantity) {
       return res.status(401).json({message: 'Digite a quantidade para o produto!'});
     }
+    if(!category) {
+      return res.status(401).json({message: 'Digite a categoria do produto!'});
+    }
     if(!bar_code) {
       return res.status(401).json({message: 'Digite o código de barras do produto!'});
     }
+    if(!req.file) {
+      return res.status(401).json({message: 'Adicione uma imagem!'});
+    }
+
+    const image = req.file;
 
     const productExists = await prismaClient.products.findUnique({where: {bar_code: bar_code}});
 
     if(productExists) {
+      fs.unlinkSync(`src/public/images/products/${req.file?.filename}`);
       return res.status(401).json({message: "Esse produto já existe!"});
     }
     await prismaClient.products.create({
@@ -44,12 +53,14 @@ export class ProductsController {
         name,
         description,
         price,
+        category,
         quantity,
         bar_code,
       }
     }).then(() => {
       return res.status(200).json({message: "Produto criado com sucesso!"});
-    }).catch((err) => {
+    }).catch((err) => { 
+      fs.unlinkSync(`src/public/images/products/${req.file?.filename}`);
       return res.status(401).json({
         message: 'Ocorreu algum erro!',
         err
@@ -84,7 +95,6 @@ export class ProductsController {
     if(!quantity) {
       return res.status(401).json({message: 'Digite a quantidade para o produto!'});
     }
-    
     await prismaClient.products.update({
       data: {
         image: image.filename,
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import './config/env';
 import verifySuperUser from './middlewares/verifySuperUser';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
